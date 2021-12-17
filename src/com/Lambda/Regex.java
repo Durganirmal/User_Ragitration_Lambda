d2360103aa509b9f637f169f6793989f481fb177
Durga Nirmal <durganirmal123@gmail.com>
2021-12-17 10:31:28 +0530

    Added:Regex Program

---
+++ src/com/Lambda/Regex.java
@@ -0,0 +1,49 @@
+package com.Lambda;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+	 public static String regex;
+
+	    public static void name(String name) {
+	        regex = "^[A-Z]{1}[a-z]{2,}$";
+	        Pattern pattern = Pattern.compile(regex);
+	        Matcher matcher = pattern.matcher(name);
+	        if (matcher.matches()) {
+	            System.out.println("\nYour name " + name + " is in correct format.");
+	        } else {
+	            System.out.println("\nYour name is not in format.");
+	            if (name.equals(User.first_name)) {
+	                User.firstname();
+	            } else if (name.equals(User.last_name)) {
+	                User.lastname();
+	            }
+	        }
+	    }
+
+	    public static void mail(String mailID) {
+	        regex = "^[a-z0-9]{1,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
+	        Pattern pattern = Pattern.compile(regex);
+	        Matcher matcher = pattern.matcher(mailID);
+	        if (matcher.matches()) {
+	            System.out.println("\nYour mailID " + mailID + " is in correct format.");
+	        } else {
+	            System.out.println("\nYour mailID is not in format.");
+	            User.mail();
+	        }
+	    }
+
+	        public static void phonenumber(String phonenumber) {
+	            regex = "^[0-9]{2}\\s{1}[0-9]{10}$";
+	            Pattern pattern = Pattern.compile(regex);
+	            Matcher matcher = pattern.matcher(phonenumber);
+	            if (matcher.matches()) {
+	                System.out.println("\nYour phone number " + phonenumber + " is in correct format.");
+	            } else {
+	                System.out.println("\nYour phone number is not in format.");
+	                User.phonenumber();
+	            }
+	        }
+	}
+
+
---
+++ src/com/Lambda/User.java
@@ -0,0 +1,40 @@
+package com.Lambda;
+import java.util.Scanner;
+public class User {
+	 public static Scanner scan = new Scanner(System.in);
+	    public static String first_name,last_name,mail_ID,phone_number;
+
+	    public static void main(String[] args) {
+	        User details = new User();
+	        details.firstname();
+	        details.lastname();
+	        details.mail();
+	        details.phonenumber();
+
+	    }
+
+
+	    public static void firstname() {
+	        System.out.println("\nEnter the first name \n first letter capital and minimum 3 letters:");
+	        first_name = scan.next();
+	        Regex.name(first_name);
+	    }
+	    public static void lastname() {
+	        System.out.println("\nEnter the last name \n first letter capital and  minimum 3 letters:");
+	        last_name = scan.next();
+	        Regex.name(last_name);
+	    }
+	    public static void mail() {
+	        System.out.println("\nEnter the mail with following valid format:");
+	        System.out.println("\nFormat is E.g. abc.xyz@bl.co.in (In that abc, bl & co is mandatory)");
+	        mail_ID = scan.next();
+	        Regex.mail(mail_ID);
+	    }
+	    public static void phonenumber() {
+	        System.out.println("\nEnter the phone number with country code and leave gap between them (E.g:91 9876543210):");
+	        phone_number = scan.nextLine();
+	        Regex.phonenumber(phone_number);
+	    }
+	}
+
+
